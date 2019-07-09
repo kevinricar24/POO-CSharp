63b71e0e40c1243c3ea66e924281300d6a17bdc6
kevin ricardo sejin <kevinricar24@hotmail.com>
2019-07-09 09:34:51 -0500

    private encapsulation example

--- POO-CSharp/POO-CSharp/Encapsulation/PrivateEncapsulation.cs
+++ POO-CSharp/POO-CSharp/Encapsulation/PrivateEncapsulation.cs
@@ -1,10 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace POO_CSharp.Encapsulation
 {
     class PrivateEncapsulation
     {
+        private double length;
+        private double width;
+
+        public void Acceptdetails()
+        {
+            Console.WriteLine("Enter Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+        }
+        private double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
     }
 }
--- POO-CSharp/POO-CSharp/Program.cs
+++ POO-CSharp/POO-CSharp/Program.cs
@@ -8,11 +8,21 @@ namespace POO_CSharp
         static void Main(string[] args)
         {
             //Public Access Specifier
+            Console.WriteLine("-----------------Public Access Specifier Example-----------------");
             PublicEncapsulation r = new PublicEncapsulation();
             r.length = 4.5;
             r.width = 3.5;
             r.Display();
             Console.ReadLine();
+
+            //Private Access Specifier
+            Console.WriteLine("-----------------Private Access Specifier Example-----------------");
+            PrivateEncapsulation r1 = new PrivateEncapsulation();
+            r1.Acceptdetails();
+            r1.Display();
+            Console.ReadLine();
+
+
         }
     }
 }
